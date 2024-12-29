c71e9775b36b28476897c71cace35e7095cf6a1e
Tracie Childers <traciechilders01@gmail.com>
2024-12-29 14:04:28 -0500

    Updated README.md and completed and commented out code and experimenting in index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,63 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+/*
+function multiply(num1, num2) {
+    return num1 * num2;
+}
 
+
+let product = multiply(2, 2);
+console.log(product); 
+
+    Output: 4
+
+
+
+function add(a, b) {
+    return a + b;
+}
+
+let sum = add(3, 4);
+console.log(sum); 
+
+    Output: 7
+
+
+
+function multiply(a, b) {
+    return a * b;
+}
+
+let product = multiply(4, 5);
+console.log(product);  
+
+    Output: 20 A Value
+
+
+
+function greet(name) {
+    console.log("Hello, " + name + "!");
+}
+
+greet("Alice"); 
+
+    Output: Hello, Alice!
+
+
+
+function add(a, b = 0) {
+    return a + b;
+}
+
+let sum = add(5);  Uses default value for the second parameter
+console.log(sum);  
+
+     Output: 5
+
+sum = add(5, 3);  Uses both provided parameters
+console.log(sum); 
+
+    Output: 8
+
+*/
