30a14cded921a5ed833a1122278b0cc5a086d773
Tracie Childers <traciechilders01@gmail.com>
2024-12-29 14:45:11 -0500

    updated README.md and created function returnTheRemainder in assignment.js

--- js/assignment.js
+++ js/assignment.js
@@ -6,3 +6,12 @@ const checkIfEvenElement = document.getElementById("check-if-even");
 const lostNumbersElement = document.getElementById("lost-numbers");
 
 const lostNumbers = [4, 8, 15, 16, 23, 42];
+
+function returnTheRemainder(num1, num2) {
+return num1 % num2;
+
+}
+let remainder = returnTheRemainder(5, 4);
+console.log(remainder);
+
+
